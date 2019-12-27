7bff79b63140fdc2c5e7aff9cb63f1de293ba8f5
kristinamiller <kfm287@gmail.com>
2019-12-26 17:55:34 -0800

    added table styling

--- src/players.jsx
+++ src/players.jsx
@@ -26,14 +26,46 @@ class Players extends React.Component {
     return (
       <div className="players-table">
         <ul className="players-ul">
-          This is the Players Table
           {this.state.players.map(player => {
             return (
-              <li key={player.player_id}>
+              <li key={player.player_id} className="player-row">
                 <ul className="player-row">
-                  {Object.values(player).map((value, idx) => {
-                    return <li key={idx} className="player-item">{value}</li>;
-                  })}
+                  <li key="1" className="player-item number">
+                    {player.player_id}
+                  </li>
+                  <li key="2" className="player-item number">
+                    {player.game_id}
+                  </li>
+                  <li key="3" className="player-item name">
+                    {player.name}
+                  </li>
+                  <li key="4" className="player-item ">
+                    {player.team_abbr}
+                  </li>
+                  <li key="5" className="player-item name">
+                    {player.team_name}
+                  </li>
+                  <li key="6" className="player-item ">
+                    {player.opp_abbr}
+                  </li>
+                  <li key="7" className="player-item name">
+                    {player.opp_name}
+                  </li>
+                  <li key="8" className="player-item points">
+                    {player.points}
+                  </li>
+                  <li key="9" className="player-item assists">
+                    {player.assists}
+                  </li>
+                  <li key="10" className="player-item rebounds">
+                    {player.rebounds}
+                  </li>
+                  <li key="11" className="player-item number">
+                    {player.starter}
+                  </li>
+                  <li key="12" className="player-item number">
+                    {player.active}
+                  </li>
                 </ul>
               </li>
             );
