08db1e08b593f58230f020a6c0b4d6e4578b4848
kristinamiller <kfm287@gmail.com>
2019-12-26 18:12:34 -0800

    player status

--- src/players.jsx
+++ src/players.jsx
@@ -26,6 +26,47 @@ class Players extends React.Component {
     return (
       <div className="players-table">
         <ul className="players-ul">
+          <li>
+            <ul className="player-row header">
+              <li key="a" className="player-item number header">
+                Player Id
+              </li>
+              <li key="b" className="player-item number header">
+                Game Id
+              </li>
+              <li key="c" className="player-item name header">
+                Name
+              </li>
+              <li key="d" className="player-item number header">
+                Team
+              </li>
+              <li key="e" className="player-item name header">
+                Team Name
+              </li>
+              <li key="f" className="player-item number header">
+                Opp 
+              </li>
+              <li key="g" className="player-item name header">
+                Opp Name
+              </li>
+              <li key="h" className="player-item number header">
+                Points
+              </li>
+              <li key="i" className="player-item number header">
+                Assists 
+              </li>
+              <li key="j" className="player-item number header">
+                Rebounds
+              </li>
+              <li key="k" className="player-item number header">
+                Starter
+              </li>
+              <li key="l" className="player-item number header">
+                Status
+              </li>
+            </ul>
+          </li>
+
           {this.state.players.map(player => {
             return (
               <li key={player.player_id} className="player-row">
@@ -64,7 +105,9 @@ class Players extends React.Component {
                     {player.starter}
                   </li>
                   <li key="12" className="player-item number">
-                    {player.active}
+                    {
+                      player.active === 1 ? "active" : "inactive"
+                    }
                   </li>
                 </ul>
               </li>
